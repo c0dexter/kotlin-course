2ecd019d8ab8c2602b22f1ad0af7d9c8022479ec
c0dexter <michal.dobrowolski1@gmail.com>
2019-09-17 20:03:35 +0200

    Lesson 18: Operations on types (Kotlit doesn't have prymitive types)

---
+++ src/com/c0dexter/TypyProsteOperacjeWstep.kt
@@ -0,0 +1,7 @@
+package com.c0dexter
+
+/*
+- W Kotlinie NIE WYSTĘPUJĄ typy proste (prymitywne)!!!
+- zmienne traktujemy jak obiekty i tak na nich pracujemy
+- wywołujemy metody jak na obiektach danego typu np. Integer (Int)
+ */

319313fb9f1f0b666efb2f0e2e88d1e59d7c9d60
c0dexter <michal.dobrowolski1@gmail.com>
2019-09-18 22:14:21 +0200

    Automatic casting - refactored code

--- src/com/c0dexter/AutomatyczneRzutowanie.kt
+++ src/com/c0dexter/AutomatyczneRzutowanie.kt
@@ -1,2 +1,26 @@
 package com.c0dexter
 
+fun main() {
+    // ### AUTOMATYCZNE RZUTOWANIE ###
+    var obj: Any = 11
+    var obj1: Any = 8
+
+    if (obj is Int) {
+        // Jeśli obiekt jest jakimś typem to wykonaj akcje...
+        // obj w tym wypadku jest juz AUTOMATYCZNIE zrzutowany na Int-a
+        // na obiekcie obj możemy wykonywać metody dostępne dla Int-a
+        obj.plus(2)
+    }
+
+    if (obj1 !is Int) { // (!) "Jeśli objekt NIE JEST Int-em..."
+        // to coś zrób...
+    } else {
+        // wtedy w ELSE mamy możliwość wykonania metod na automatycznie zrzutowanym obiekcie
+        obj1.div(4)
+    }
+
+
+    // ### WYMUSZENIE RZUTOWANIA NA SPODZIEWANY TYP - niebezpieczne przy pomyłce -> błąd ###
+    var jakasNowaZmiennaInt: Int = obj1 as Int // potraktuj zminną obj1 JAKO(AS) Int
+
+}
--- src/com/c0dexter/Enter.kt
+++ src/com/c0dexter/Enter.kt
@@ -4,27 +4,6 @@ object Enter {
     @JvmStatic
     fun main(args: Array<String>) {
 
-        // ### AUTOMATYCZNE RZUTOWANIE ###
-        var obj: Any = 11
-        var obj1: Any = 8
-
-        if(obj is Int){
-            // Jeśli obiekt jest jakimś typem to wykonaj akcje...
-            // obj w tym wypadku jest juz AUTOMATYCZNIE zrzutowany na Int-a
-            // na obiekcie obj możemy wykonywać metody dostępne dla Int-a
-            obj.plus(2)
-        }
-
-        if (obj1 !is Int){ // (!) "Jeśli objekt NIE JEST Int-em..."
-            // to coś zrób...
-        } else{
-            // wtedy w ELSE mamy możliwość wykonania metod na automatycznie zrzutowanym obiekcie
-            obj1.div(4)
-        }
-
-
-        // ### WYMUSZENIE RZUTOWANIA NA SPODZIEWANY TYP - niebezpieczne przy pomyłce -> błąd ###
-        var jakasNowaZmiennaInt: Int = obj1 as Int // potraktuj zminną obj1 JAKO(AS) Int
 
     }
 }

8003c02d25d58b152ccd26b04bd5a4cfc0748cf3
c0dexter <michal.dobrowolski1@gmail.com>
2019-09-18 23:22:58 +0200

    Bit operators - Kotlin vs Java

--- src/com/c0dexter/OperatoryBitowe.kt
+++ src/com/c0dexter/OperatoryBitowe.kt
@@ -1,2 +1,13 @@
 package com.c0dexter
 
+fun main(){
+    // shl - shift left             ---java--->  <<
+    // shr - shift right            ---java--->  >>
+    // ushr unsigned shift right    ---java--->  >>>
+    // ushr unsigned shift left     ---java--->  <<<
+
+    // && / and     ------> logiczne / binarne
+    // || / or      ------> logiczne / binarne
+    // ^ / xor      ------> logiczne / binarne
+    // ~ / inv      ------> logiczne / binarne
+}

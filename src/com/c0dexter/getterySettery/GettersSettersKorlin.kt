5040889d618125d959150a8225ecdfd699aa8484
c0dexter <michal.dobrowolski1@gmail.com>
2019-09-24 21:42:18 +0200

    Lesson 38: Where are the class fields known from Java?

--- src/com/c0dexter/getterySettery/GettersSettersKorlin.kt
+++ src/com/c0dexter/getterySettery/GettersSettersKorlin.kt
@@ -10,9 +10,37 @@ PROPERCJE W KOTLINIE"
 
 class GettersSettersKorlin {
     // -- PROPERCJE, CECHY I WŁASNOŚCI KLASY TRZYMAMY W PROPERCJACH -- //
-    var name: String = "Piotr"
-    var isMale: Boolean = true
-    val size: Int get() {return name.length}
+
+    /*
+    POLE JEST TWORZONE DLA PROPERCJI GDY:
+    - propercja ma domyślny przynajmniej jeden getter albo sertter
+    - albo doprecyzowany getter albo setter który odwołuje się do pola 'field'
+     */
+
+
+    var name: String = "Piotr"  // Za tą propercją kryje się pole, ponieważ jest bezpośrednia inicjalizacja
+
+
+    var isMale: Boolean = true  // propercja isMale ma za sobą pole
+    set(value) {
+        if(value != field){     // do tego pola odwołujemy się poprzez słowo kluczowe 'field'
+        field = value
+        }
+    }
+
+    get() {
+        if(field != null){
+        return true
+    } else{
+        return false
+    }
+    }
+
+
+    // W tym przypadku nie ma żadnego odłowania do field, więc nie potrzebne jest pole 'field'
+    val size: Int get() {
+        return name.length
+    }
 
 
     // -- LOGIKĘ KLASY, JEJ ZADANIA I ZACHOWANIA TRZYMAMY W FUNKCJACH -- //

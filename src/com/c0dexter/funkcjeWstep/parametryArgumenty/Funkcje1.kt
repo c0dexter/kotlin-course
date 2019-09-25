d9a267e7da4d95006333cfeb6645683ed9c3761e
c0dexter <michal.dobrowolski1@gmail.com>
2019-09-25 23:19:12 +0200

    Lesson 42: Parameters and default arguments in functions

---
+++ src/com/c0dexter/funkcjeWstep/parametryArgumenty/Funkcje1.kt
@@ -0,0 +1,16 @@
+package com.c0dexter.funkcjeWstep
+
+open class Funkcje1{
+
+    open fun printHello(textToPrint: String, liczba: Int = 5){   // zainicjalizowanie argumentu powoduje jego opcjonalność przy wywołaniu metody
+    println("Hello! So much fun! $textToPrint Liczba: $liczba")
+    }
+}
+
+class NoweFunkcje: Funkcje1(){
+
+    override fun printHello(textToPrint: String, liczba: Int){   // nie można nadpisać argumentu domyślnego, należy usunąć inicjalizację
+    println("Hello! So much fun! $textToPrint Liczba: $liczba")
+    }
+
+}
---
+++ src/com/c0dexter/funkcjeWstep/parametryArgumenty/FunkcjeMain1.kt
@@ -0,0 +1,10 @@
+package com.c0dexter.funkcjeWstep
+
+object FunkcjeMain1 {
+
+    @JvmStatic
+    fun main(args: Array<String>){
+        Funkcje1().printHello("Michał") // Wykorzystanie argumentu domyślnego liczba: Int = 5
+        Funkcje1().printHello("Michał", 2)
+    }
+}

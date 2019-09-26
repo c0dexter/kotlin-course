a56d6ed1ac3d0cd38c093b5be8c5622a2363eeda
c0dexter <michal.dobrowolski1@gmail.com>
2019-09-26 15:22:08 +0200

    Lesson 43: Named args in functions

---
+++ src/com/c0dexter/funkcjeWstep/nazywaneArgumenty/Funkcje2.kt
@@ -0,0 +1,28 @@
+package com.c0dexter.funkcjeWstep
+
+open class Funkcje2 {
+
+    open fun printHello(
+        textToPrint: String,
+        liczba: Int = 5,
+        innyInt: Int = 6,
+        jakisString: String = "Test",
+        vararg stringi: String
+    ) { // vararg - lista argumentów z kropkami (Java)
+        println("Hello! So much fun! $textToPrint Liczba: $liczba")
+    }
+}
+
+class NowaKlasa : Funkcje2() {
+
+    override fun printHello(
+        textToPrint: String,
+        liczba: Int,
+        innyInt: Int,
+        jakisString: String,
+        vararg stringi: String
+    ) {   // nie można nadpisać argumentu domyślnego, należy usunąć inicjalizację
+        println("Hello! So much fun! $textToPrint Liczba: $liczba, inna liczba: $innyInt, tekst: $jakisString")
+    }
+
+}
---
+++ src/com/c0dexter/funkcjeWstep/nazywaneArgumenty/FunkcjeMain2.kt
@@ -0,0 +1,21 @@
+package com.c0dexter.funkcjeWstep
+
+object FunkcjeMain2 {
+
+    @JvmStatic
+    fun main(args: Array<String>){
+        NowaKlasa().printHello("Michał") // Wykorzystanie argumentu domyślnego liczba: Int = 5
+        NowaKlasa().printHello("Michał", 2)
+        NowaKlasa().printHello("Michał", 2, innyInt = 3, jakisString = "cośTam") // Argumenty nazwane
+        NowaKlasa().printHello("Michał", 2, innyInt = 3, jakisString = "cośTam", stringi = *arrayOf("a", "b", "c"))
+
+        /*
+        ARGUMENTY NAZWANE:
+        - to argumenty z wartością domyślną, które są opcjonalne, jednak wartość jest przypisana po podaniu ich nazwy
+        - argumenty wymagane muszą być podane jako pierwsze, nie można mieszać kolejności np. (nazwany, nienazwany, nazwany, nienazwany, nienazwany) (CTRL+P)
+        - brak wypisania argumentu nazwanego powoduje jego pominięcie
+        - przypiasnie wartości do argumentu nazwanego wymaga podania jego nazwy
+        - po podaniu nazwy któregoś z argumentów nazwanych musimy podać nazwy pozostałych argumentów nazwanych
+         */
+    }
+}

77e0a51d18db7ab40519a307057b2ed2bff58350
c0dexter <michal.dobrowolski1@gmail.com>
2019-09-30 22:35:19 +0200

    Lesson 51: Properties in interfaces

---
+++ src/com/c0dexter/funkcje/interfejsy/propercjeWInterfejsach/Implementacja.kt
@@ -0,0 +1,7 @@
+package com.c0dexter.funkcje.interfejsy
+
+class Implementacja1: Interfejs1 {
+    override fun abstrakcyjna() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}

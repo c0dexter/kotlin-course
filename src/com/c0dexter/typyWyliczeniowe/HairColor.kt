ecfd6f3cf91137f50dff0f937e54492b4ee80580
c0dexter <michal.dobrowolski1@gmail.com>
2019-10-04 00:00:33 +0200

    Lesson 65: ENUM type

---
+++ src/com/c0dexter/typyWyliczeniowe/HairColor.kt
@@ -0,0 +1,14 @@
+package com.c0dexter.typyWyliczeniowe
+
+enum class HairColor(val rgb: Int) {
+    BLOND(rgb = 0xfaf0be),
+    RUDE(rgb = 0xb06500),
+    CZARNE(rgb = 0x000000),
+    POPIELATE(rgb = 0xaaaaaa)
+}
+
+/*
+W Kotlinie enumy mogą być instancjonowane z jakąś wartością
+- można więc zmapować sobie w tym przypadku kolory na stałe
+- odwołując się później do danego skłądnika enuma mamy dostęp bezpośrednio do jego wartości
+ */
